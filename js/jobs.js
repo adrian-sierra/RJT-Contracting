b71f3e81372fd3181a5b94090450115fff46bf71
Adrian Sierra <87449902+adrian-sierra@users.noreply.github.com>
2022-06-01 09:58:15 -0700

    Update jobs.js

--- js/jobs.js
+++ js/jobs.js
@@ -1,8 +1,10 @@
-const navigation = document.querySelector("#banner-nav");
+const navigation = document.querySelector("#banner-nav"); // set up for DOM manipulation for nav bar and navigation
 const navigationButton = document.querySelector(".banner-nav-toggle");
 
 navigationButton.addEventListener("click", () => {
+  // event listener for the navigation bar when the window is in a phone sized screen
   if (navigation.getAttribute("data-visible") === "false") {
+    // this if statements and the code inside will alter based off its current condition and the result is the side bar will expand out to the screen or slide out of the screen
     navigation.setAttribute("data-visible", "true");
     navigationButton.setAttribute("aria-expanded", "true");
     navigationButton.classList.remove("fa-bars");
@@ -15,10 +17,11 @@ navigationButton.addEventListener("click", () => {
   }
 });
 
-const langDownArrow = document.getElementById("language-down-arrow");
+const langDownArrow = document.getElementById("language-down-arrow"); // set up for DOM manipulation for the selected language
 const otherLangContainer = document.getElementById("other-lang");
 
 langDownArrow.addEventListener("click", () => {
+  // another event listenener for the language selector button and the conditions are then set accordingly
   if (otherLangContainer.getAttribute("data-visible") === "false") {
     otherLangContainer.setAttribute("data-visible", "true");
     langDownArrow.classList.remove("fa-angle-down");
@@ -30,12 +33,12 @@ langDownArrow.addEventListener("click", () => {
   }
 });
 
-const workForm = document.querySelector(".form-work");
+const workForm = document.querySelector(".form-work"); // this section deals with the user adding more work experience by clicking the button that states "add work experience" and the associated Google Maps API for autocomplete
 const workExperience = document.querySelector(".work-experience");
 const addWorkButton = document.getElementById("add-work");
 let totalWorkExperience = parseInt(
   document.getElementById("add-work-value").value
-);
+); // tracker for the work experience forms on the page in order to associate the correct element IDs with the number of forms
 //console.log(totalWorkExperience);
 let workStringTemplate = [
   "work-company-",
@@ -45,15 +48,17 @@ let workStringTemplate = [
   "work-zip-",
   "work-start-",
   "work-end-",
-];
+]; // template to associate the element IDs with the corresponding number
 
 addWorkButton.addEventListener("click", () => {
+  // event listener to add work experience form as well as adding independent functionality to this form for Google Maps API as well as clearing any of the information adding to previous forms
   totalWorkExperience += 1;
   document.getElementById("add-work-value").value = totalWorkExperience;
   //console.log(document.getElementById("add-work-value").value);
-  let workExperienceCopy = workExperience.cloneNode(true);
+  let workExperienceCopy = workExperience.cloneNode(true); // used to clone another work experience form element
   let inputs = workExperienceCopy.getElementsByClassName("jobs-input");
   for (let i = 0; i < inputs.length; i++) {
+    // this loop is used to go through the inputs on the work experience form and the names of this form and set them to the appropriate names based on the number of work experiences that were incremented as well as resetting the inputs if they were filled in the previous forms
     let concatString = workStringTemplate[i] + totalWorkExperience.toString();
     inputs[i].id = concatString;
     inputs[i].setAttribute("name", concatString);
@@ -62,17 +67,20 @@ addWorkButton.addEventListener("click", () => {
     }
   }
   workForm.removeChild(addWorkButton);
-  workForm.appendChild(workExperienceCopy);
+  workForm.appendChild(workExperienceCopy); // add the new work experience form to a work form container
 
-  let addressQuery = "#" + inputs[3].id;
+  let addressQuery = "#" + inputs[3].id; // used to select the appropriate elements for Google Maps API
   let zipQuery = "#" + inputs[4].id;
-  let newWorkField = workExperienceCopy.querySelector(addressQuery);
-  let newZipField = workExperienceCopy.querySelector(zipQuery);
-  let newAutocomplete = new google.maps.places.Autocomplete(
+  let newWorkField = workExperienceCopy.querySelector(addressQuery); // have to select the appropriate address input
+  let newZipField = workExperienceCopy.querySelector(zipQuery); // have to select the appropriate zip code input
+  let newAutocomplete = new google.maps.places.Autocomplete( // create new object of the Google Maps API Autocompete and pass in the corresponding address input and default options
     newWorkField,
     options
   );
   newAutocomplete.addListener("place_changed", () => {
+    // use the addListener method on the newly created object which is used with the "place_changed" argument to get the new place based on input, and uses an arrow function to get the corresponding data from the results
+
+    // most all of this structure came from the documentation for the Autocomplete functionality from Google
     const place = newAutocomplete.getPlace();
     let address1 = "";
     let postcode = "";
@@ -109,11 +117,11 @@ addWorkButton.addEventListener("click", () => {
     }
   });
   if (totalWorkExperience < 3) {
-    workForm.appendChild(addWorkButton);
+    workForm.appendChild(addWorkButton); // DOM control based on number of work experience forms
   }
 });
 
-const referenceForm = document.querySelector(".form-references");
+const referenceForm = document.querySelector(".form-references"); // references form and DOM manipulation works almost exactly like it was used for the work experience with just updated templates and elements
 const reference = document.querySelector(".reference");
 const addReferenceButton = document.getElementById("add-reference");
 
@@ -131,6 +139,7 @@ let referenceStringTemplate = [
 ];
 
 addReferenceButton.addEventListener("click", () => {
+  // same as the event listener for work experience
   totalReferences += 1;
   document.getElementById("add-reference-value").value = totalReferences;
   //console.log(document.getElementById("add-reference-value").value);
@@ -151,7 +160,7 @@ addReferenceButton.addEventListener("click", () => {
   }
 });
 
-let streetAutocomplete;
+let streetAutocomplete; // this section and variables work like the work experience forms that use the Google Maps API except that this part is for the personal info and corresponding address
 let workAutocomplete;
 let streetField;
 let streetZipField;
@@ -161,9 +170,10 @@ let options = {
   componentRestrictions: { country: ["us", "ca"] },
   fields: ["address_components", "geometry"],
   types: ["address"],
-};
+}; // option for the results of the Google Maps API
 
 function initAutocomplete() {
+  // same as the above in the work experience form with the only exception being that it is ready to be used at the moment of input
   streetField = document.querySelector("#street-address");
   streetZipField = document.querySelector("#street-zip");
   streetAutocomplete = new google.maps.places.Autocomplete(
@@ -208,7 +218,7 @@ function initAutocomplete() {
     streetZipField.value = postcode;
   });
 
-  workField = document.querySelector("#work-address-1");
+  workField = document.querySelector("#work-address-1"); // like the rest of the Autocomplete forms, this is just independent since it is always present on the page regardless of the user adding more forms
   workZipField = document.querySelector("#work-zip-1");
   workAutocomplete = new google.maps.places.Autocomplete(workField, options);
   workAutocomplete.addListener("place_changed", () => {
@@ -250,4 +260,4 @@ function initAutocomplete() {
   });
 }
 
-window.initAutocomplete = initAutocomplete;
+window.initAutocomplete = initAutocomplete; // here we instantiate the Autocomplete functionality on the current window
